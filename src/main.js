0c28f3fd70653277492afc80ba62f890d0659f29
Vikas94a <vikas1.kodehode@gmail.com>
2024-12-04 18:33:01 +0100

    scroll-effect

--- src/main.js
+++ src/main.js
@@ -5,31 +5,27 @@ import { setupCounter } from "./counter.js";
 
 import { gsap } from "gsap";
 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import { Draggable } from "gsap/Draggable";
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#circle", {
+  scrollTrigger: {
+    trigger: "#circle",
+    start: "top ",
+    end: "bottom ",
+    scrub: true,
+    markers: true,
+  },
+  y: 600,
+  rotate: 360,
+});
 
 gsap.registerPlugin(Draggable);
 Draggable.create(".box1");
 Draggable.create(".box2");
 Draggable.create(".box3");
 Draggable.create(".box4");
-// Draggable.create(".container");
+Draggable.create("#circle");
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
 
-// setupCounter(document.querySelector('#counter'))
