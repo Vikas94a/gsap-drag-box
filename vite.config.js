daf461f25fd35d18d4d7ca4f0c9bd5816ab84d3e
Vikas94a <vikas1.kodehode@gmail.com>
2024-12-09 11:32:24 +0100

    changes for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Vikas94a/gsap-drag-box.git",
+  base: "/gsap-drag-box/",
+  // base: "https://github.com/Vikas94a/gsap-drag-box.git",
 });
